fa7d67638f070537c81a2ddd388e3fbafa865707
OmegaIdealElectric <OmegaIdealElectric@gmail.com>
2023-01-15 23:29:24 -0500

    Initial commit

--- helper.ps1
+++ helper.ps1
@@ -1,5 +1,10 @@
+git status 
+
 git add --all
 
 git commit -m "Initial commit"
 
 git push -u origin main
+
+git config --global user.email "OmegaIdealElectric@gmail.com"
+git config --global user.name "OmegaIdealElectric"
